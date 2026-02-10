1b4b12d4f09917dfdb44e5a35b77a54fe4ad2cc9
hmmunna72-prog <hmmunna72@gmail.com>
2026-02-10 04:22:00 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,4 +1,5 @@
 window.QUESTIONS = [
+
   {
     id: "Q1",
     text: "Taksin kyytiin tulee hyvin erilaisia ja eri-ikäisiä asiakkaita. Mitä sinun on hyvä muistaa lapsista taksissa?",
@@ -9,6 +10,7 @@ window.QUESTIONS = [
     ],
     answerIndex: 0
   },
+
   {
     id: "Q2",
     text: "Asiakas pyytää kiireessä ajamaan nopeasti ja ohittamaan turvallisuusasiat. Mitä teet?",
@@ -18,5 +20,28 @@ window.QUESTIONS = [
       "Noudatan aina liikennesääntöjä ja varmistan turvallisen kyydin."
     ],
     answerIndex: 2
+  },
+
+  {
+    id: "Q3",
+    text: "Saako taksinkuljettaja käyttää alkoholia ennen ajovuoroa?",
+    options: [
+      "Saa käyttää vähän.",
+      "Ei saa käyttää lainkaan.",
+      "Saa käyttää jos asiakas sallii."
+    ],
+    answerIndex: 1
+  },
+
+  {
+    id: "Q4",
+    text: "Kuka vastaa matkustajien turvallisuudesta taksissa?",
+    options: [
+      "Matkustaja itse.",
+      "Taksinkuljettaja.",
+      "Poliisi."
+    ],
+    answerIndex: 1
   }
+
 ];
