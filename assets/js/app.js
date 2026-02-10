e081fc5a0f974c7b8c5cfa998bdbf0734ced9e62
Mahmudul Hasan Munna <hmmunna72@gmail.com>
2026-02-10 08:11:51 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
-
+function openSidebar() {
+  document.getElementById("sidebar").classList.add("open");
+  document.getElementById("overlay").classList.add("show");
+}
+function closeSidebar() {
+  document.getElementById("sidebar").classList.remove("open");
+  document.getElementById("overlay").classList.remove("show");
+}
