af785dfea2ef8c10c9490cb496fc68a8d37a3ac8
hmmunna72-prog <hmmunna72@gmail.com>
2026-02-10 04:13:04 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,22 @@
-
+window.QUESTIONS = [
+  {
+    id: "Q1",
+    text: "Taksin kyytiin tulee hyvin erilaisia ja eri-ikäisiä asiakkaita. Mitä sinun on hyvä muistaa lapsista taksissa?",
+    options: [
+      "Lapselle, jonka pituus on alle 135 cm, on käytettävä turvalaitetta.",
+      "Alle 135 cm lapsi saa aina matkustaa ilman turvalaitetta.",
+      "Lapsi saa aina istua etupenkillä ilman turvalaitetta."
+    ],
+    answerIndex: 0
+  },
+  {
+    id: "Q2",
+    text: "Asiakas pyytää kiireessä ajamaan nopeasti ja ohittamaan turvallisuusasiat. Mitä teet?",
+    options: [
+      "Teen niin kuin asiakas käskee.",
+      "Ajan nopeammin, koska asiakas on vastuussa.",
+      "Noudatan aina liikennesääntöjä ja varmistan turvallisen kyydin."
+    ],
+    answerIndex: 2
+  }
+];
